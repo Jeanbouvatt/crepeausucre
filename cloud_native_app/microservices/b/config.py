042363b49cc524c69407e727e93b70713c254289
Salim Aboubacar <salimaboubacar@gmail.com>
2017-01-16 12:22:12 +0100

    Microservice B done

--- cloud_native_app/microservices/b/config.py
+++ cloud_native_app/microservices/b/config.py
@@ -1,52 +1,34 @@
 # coding=utf-8
 
 import configparser
-import mysql.connector 
 
 # Initialise global variable
 logger = None
-i = None
+b = None
 
 
-def initialise_i():
+def initialise_b():
     """Define i global object so it can be called from anywhere."""
-    global i
-    i = I()
+    global b
+    b = B()
 
 
-class I(object):
+class B(object):
     def __init__(self):
-        self.NAME = "Microservice i"
+        self.NAME = "Microservice b"
         self.VERSION = "0.1"
         #
         # Configuration file
-        self.conf_file = iConfiguration("i.conf")
-        #self.mysql = mysql.connector.connect(host=self.conf_file.get_sql_hostname(), \
-         #                                    user=self.conf_file.get_sql_username(), \
-         #                                    password=self.conf_file.get_sql_password(), \
-          #                                   database=self.conf_file.get_sql_database())
+        self.conf_file = bConfiguration("b.conf")
 
-
-class iConfiguration(object):
+class bConfiguration(object):
 
     def __init__(self, configuration_file):
         self.config = configparser.ConfigParser(allow_no_value=True)
         self.config.read(configuration_file)
 
-    def get_i_port(self):
-        return self.config.get("i", "port")
+    def get_b_port(self):
+        return self.config.get("b", "port")
 
-    def get_sql_hostname(self):
-        return self.config.get("sql", "hostname")
-
-    def get_sql_username(self):
-        return self.config.get("sql", "username")
-
-    def get_sql_password(self):
-        return self.config.get("sql", "password")
-
-    def get_sql_database(self):
-        return self.config.get("sql", "database")
-
-    def get_i_debug(self):
-        return self.config.get("i", "debug")
+    def get_b_debug(self):
+        return self.config.get("b", "debug")
