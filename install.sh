a1e66e10d940b05cd9a94f010283380202ce0cb0
JeanBouvatt <jeanbouvatt@gmail.com>
2017-01-17 12:43:15 +0000

    hygbjhb

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+source ./openrc.sh
 echo $1
 FLAVOR="m1.tiny"
 NIC="net-id=a992853b-b9cf-42c7-9b7e-e3e8fecd58fb"
@@ -9,6 +9,7 @@ SECURITY="default"
 
 openstack keypair create KEY_NAME > MY_KEY.pem
 chmod 600 MY_KEY.pem
+#TODO : security things
 #creating i s b w p frontend
 openstack server create --flavor $FLAVOR --image $IMAGE --nic $NIC --security-group $SECURITY sub_i --key-name $KEY_NAME
 openstack server create --flavor $FLAVOR --image $IMAGE --nic $NIC --security-group $SECURITY sub_s --key-name $KEY_NAME
@@ -16,18 +17,18 @@ openstack server create --flavor $FLAVOR --image $IMAGE --nic $NIC --security-gr
 openstack server create --flavor $FLAVOR --image $IMAGE --nic $NIC --security-group $SECURITY sub_w --key-name $KEY_NAME
 openstack server create --flavor $FLAVOR --image $IMAGE --nic $NIC --security-group $SECURITY sub_p --key-name $KEY_NAME
 openstack server create --flavor $FLAVOR --image $IMAGE --nic $NIC --security-group $SECURITY frontend --key-name $KEY_NAME
-
+DIR="cloud_native_app/microservices"
 #TODO retrieve IP address from i s b w p frontend
-scp -i MY_KEY.pem -r i $IMAGE@$IP_I
-scp -i MY_KEY.pem -r s $IMAGE@$IP_S
-scp -i MY_KEY.pem -r b $IMAGE@$IP_B
-scp -i MY_KEY.pem -r w $IMAGE@$IP_W
-scp -i MY_KEY.pem -r p $IMAGE@$IP_P
-scp -i MY_KEY.pem -r frontend IMAGE@$IP_FRONTEND
+scp -i MY_KEY.pem -r $DIR/i $IMAGE@$IP_I
+scp -i MY_KEY.pem -r $DIR/s $IMAGE@$IP_S
+scp -i MY_KEY.pem -r $DIR/b $IMAGE@$IP_B
+scp -i MY_KEY.pem -r $DIR/w $IMAGE@$IP_W
+scp -i MY_KEY.pem -r $DIR/p $IMAGE@$IP_P
+scp -i MY_KEY.pem -r #TODO frontend $IMAGE@$IP_FRONTEND
 
-ssh -i MY_KEY.pem $IMAGE@$IP_I 'bash -s' < install_i.sh
-ssh -i MY_KEY.pem $IMAGE@$IP_S 'bash -s' < install_s.sh
-ssh -i MY_KEY.pem $IMAGE@$IP_B 'bash -s' < install_b.sh
-ssh -i MY_KEY.pem $IMAGE@$IP_W 'bash -s' < install_w.sh
-ssh -i MY_KEY.pem $IMAGE@$IP_P 'bash -s' < install_p.sh
-ssh -i MY_KEY.pem $IMAGE@$IP_FRONTEND 'bash -s' < install_frontend.sh
+ssh -i MY_KEY.pem $IMAGE@$IP_I 'bash -s' < $DIR/i/install.sh
+ssh -i MY_KEY.pem $IMAGE@$IP_S 'bash -s' < $DIR/s/install.sh
+ssh -i MY_KEY.pem $IMAGE@$IP_B 'bash -s' < $DIR/b/install.sh
+ssh -i MY_KEY.pem $IMAGE@$IP_W 'bash -s' < $DIR/w/install.sh
+ssh -i MY_KEY.pem $IMAGE@$IP_P 'bash -s' < $DIR/p/install.sh
+ssh -i MY_KEY.pem $IMAGE@$IP_FRONTEND 'bash -s' < #TODO$DIR/install.sh
