2b22b9ad605d11f19fab8f3432efa1c58c1c7ebc
bouvattj <jeanbouvatt@gmail.com>
2017-01-20 15:37:57 +0100

    Modularisation du code

--- install.sh
+++ install.sh
@@ -6,39 +6,21 @@ NIC="net-id=a992853b-b9cf-42c7-9b7e-e3e8fecd58fb"
 IMAGE=ubuntu1604
 KEY_NAME="KEY_NAME"
 SECURITY="default"
-
-openstack keypair create KEY_NAME > MY_KEY.pem
-chmod 600 MY_KEY.pem
+if [ "$1" == "gen_key" ]; then
+	openstack keypair create KEY_NAME > MY_KEY.pem
+	chmod 600 MY_KEY.pem
+fi
 #TODO : security things
 #creating i s b w p frontend
-openstack server create --flavor $FLAVOR --image $IMAGE --nic $NIC --security-group $SECURITY sub_i --key-name $KEY_NAME
-openstack server create --flavor $FLAVOR --image $IMAGE --nic $NIC --security-group $SECURITY sub_s --key-name $KEY_NAME
-openstack server create --flavor $FLAVOR --image $IMAGE --nic $NIC --security-group $SECURITY sub_b --key-name $KEY_NAME
-openstack server create --flavor $FLAVOR --image $IMAGE --nic $NIC --security-group $SECURITY sub_w --key-name $KEY_NAME
-openstack server create --flavor $FLAVOR --image $IMAGE --nic $NIC --security-group $SECURITY sub_p --key-name $KEY_NAME
-openstack server create --flavor $FLAVOR --image $IMAGE --nic $NIC --security-group $SECURITY frontend --key-name $KEY_NAME
-
-sleep 2m 
-
+if [ "$1" == "test" ]; then
+	  echo "testing script"
+fi
 DIR="cloud_native_app/microservices"
-#TODO retrieve IP address from i s b w p frontend
-sleep 10
-IP_I= openstack server list | grep sub_i | cut -d = -f 2
-IP_S= openstack server list | grep sub_s | cut -d = -f 2
-IP_B= openstack server list | grep sub_b | cut -d = -f 2
-IP_W= openstack server list | grep sub_w | cut -d = -f 2
-IP_P= openstack server list | grep sub_p | cut -d = -f 2
+./setup_service.sh "server_i" "default" $($DIR/i) $KEY_NAME $($DIR/i/install.sh) 
+#./setup_service.sh "server_s" "default" $($DIR/s) $KEY_NAME $($DIR/s/install.sh) 
+#./setup_service.sh "server_b" "default" $($DIR/b) $KEY_NAME $($DIR/b/install.sh) 
+#./setup_service.sh "server_w" "default" $($DIR/w) $KEY_NAME $($DIR/w/install.sh) 
+#./setup_service.sh "server_p" "default" $($DIR/p) $KEY_NAME $($DIR/p/install.sh) 
+#./setup_service.sh "server_frontend" "default" $(cloud_native_app/frontend) $KEY_NAME $(cloud_native_app/frontend/install.sh) 
+echo "done"
 
-scp -i MY_KEY.pem -r $DIR/i $IMAGE@$IP_I
-scp -i MY_KEY.pem -r $DIR/s $IMAGE@$IP_S
-scp -i MY_KEY.pem -r $DIR/b $IMAGE@$IP_B
-scp -i MY_KEY.pem -r $DIR/w $IMAGE@$IP_W
-scp -i MY_KEY.pem -r $DIR/p $IMAGE@$IP_P
-scp -i MY_KEY.pem -r 'cloud_native_app/frontend' $IMAGE@$IP_FRONTEND
-
-ssh -i MY_KEY.pem $IMAGE@$IP_I 'bash -s' < $DIR/i/install.sh
-ssh -i MY_KEY.pem $IMAGE@$IP_S 'bash -s' < $DIR/s/install.sh
-ssh -i MY_KEY.pem $IMAGE@$IP_B 'bash -s' < $DIR/b/install.sh
-ssh -i MY_KEY.pem $IMAGE@$IP_W 'bash -s' < $DIR/w/install.sh
-ssh -i MY_KEY.pem $IMAGE@$IP_P 'bash -s' < $DIR/p/install.sh
-ssh -i MY_KEY.pem $IMAGE@$IP_FRONTEND 'bash -s' < 'cloud_native_app/frontend/install.sh'
