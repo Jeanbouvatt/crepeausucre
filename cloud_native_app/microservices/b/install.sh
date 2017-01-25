acc167c59a5c728f71ffeeb5562d5c2192884895
Faellan <salimaboubacar@gmail.com>
2017-01-25 11:40:12 +0100

    Update install.sh

--- cloud_native_app/microservices/b/install.sh
+++ cloud_native_app/microservices/b/install.sh
@@ -11,4 +11,4 @@ sudo apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual
 sudo apt-get install docker-engine
 sudo service docker start
 sudo docker build -t microservice_i .
-sudo docker run -it -p 8093:8093 microservice_i
+sudo docker run -it -p 8093:8093 microservice_i &
