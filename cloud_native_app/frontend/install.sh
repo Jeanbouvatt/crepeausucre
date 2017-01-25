00cdb36e34490d51e9ace9ee0a3e23182fb52284
Faellan <salimaboubacar@gmail.com>
2017-01-25 10:59:40 +0100

    Update install.sh

--- cloud_native_app/frontend/install.sh
+++ cloud_native_app/frontend/install.sh
@@ -1,14 +1,6 @@
 cd frontend
 sudo apt-get update
-sudo apt-get install apt-transport-https ca-certificates
-sudo apt-key adv \
-               --keyserver hkp://ha.pool.sks-keyservers.net:80 \
-               --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | sudo tee /etc/apt/sources.list.d/docker.list
-sudo apt-get update
-sudo apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual
-
-sudo apt-get install docker-engine
-sudo service docker start
-sudo docker build -t frontend .
-sudo docker run --rm -it -p 80:80 frontend
+sudo apt-get install -y php5 apache2 php5-curl
+cp -r www/ /var/www/html
+rm /var/www/html/index.html
+./etc/init.d/apache2 start
