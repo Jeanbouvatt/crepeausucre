5ad286d29964f129987c3c1efc517061d926db23
Faellan <salimaboubacar@gmail.com>
2017-01-25 11:13:02 +0100

    Update install.sh

--- cloud_native_app/frontend/install.sh
+++ cloud_native_app/frontend/install.sh
@@ -1,7 +1,7 @@
-mkdir -p /var/www/html
+sudo mkdir -p /var/www/html
 cd frontend
 sudo apt-get update
-sudo apt-get install -y php5 apache2 php5-curl
+sudo apt-get install -y php apache2 php-curl
 sudo cp -r www/ /var/www/html
 sudo rm /var/www/html/index.html
 ./etc/init.d/apache2 start
